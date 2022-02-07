475e2a7e8bbadd6b14420a19452025700177b719
adbray <anth.bray@outlook.com>
2022-02-06 22:28:13 -0600

    Changed POST request to PUT request

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -86,7 +86,7 @@ public class Bot extends ListenerAdapter {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://localhost:8080/user?id=" + id + "&name=" +name))
                 .timeout(Duration.ofSeconds(5))
-                .POST(HttpRequest.BodyPublishers.noBody())
+                .PUT(HttpRequest.BodyPublishers.noBody())
                 .build();
         HttpClient client = HttpClient.newBuilder().build();
         try {
